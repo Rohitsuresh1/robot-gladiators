bdfdbb270dc68c4c4e5488bc3f87076acef3d30a
Rohitsuresh1 <99383088+Rohitsuresh1@users.noreply.github.com>
2022-03-29 11:04:33 -0400

    changed if statement

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,15 +1,43 @@
 // window.alert("This is an alert! JavaScript is running!");
 // This creates a function named "fight"
 
-function fight() {
-    window.alert("The fight has begun! ");
-}
+// function fight() {
+//     window.alert("The fight has begun! ");
+// }
 // fight();
 
 var playerName = window.prompt("What is your robot's name?");
-console.log(playerName);
+var playerHealth=100;
+var playerAttack=10;
 
-console.log("This logs a string, good for leaving yourself a message");
-console.log(10+10);
-console.log("Our Robot's name is " + playerName);
+var enemyName ="Roborto";
+var enemyHealth=50;
+var enemyAttack=12;
 
+var fight = function() {
+    window.alert("Welcome to Robot Gladiators!");
+    enemyHealth -= playerAttack;
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+      );
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " has died!");
+      } 
+      else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+      }
+    if (playerHealth <= 0) {
+        window.alert(playerName + " has died!");
+      } 
+    else {
+        window.alert(playerName + " still has " + playerHealth + " health left.");
+      }
+    playerHealth -= enemyAttack;
+    console.log(playerHealth);
+};
+
+fight();
+
+if (playerHealth > 0) {
+    console.log("Your player is still alive!");
+  }
